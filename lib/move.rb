2f137b123f76082f90c7cf7923c31045483124a1
ABHISHEK SATHE <abhisathe96@gmail.com>
2018-01-09 07:20:07 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 def input_to_index(input)
   index = input.to_i - 1
 end
-def move(board , index , player)
+def move(board , index , player="X")
   board[index] = "#{player}"
   display_board(board)
 end
